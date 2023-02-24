206d29d99b781b16b307c613a6543ab39b342f8a
tkachenko <smokiei473@gmail.com>
2023-02-24 12:34:17 +0200

    test breaker

--- src/routes.py
+++ src/routes.py
@@ -6,6 +6,7 @@ from src.resources.films import FilmListApi
 from src.resources.smoke import Smoke
 
 api.add_resource(Smoke, '/smoke321', strict_slashes=False)
+
 api.add_resource(FilmListApi, '/films', '/films/<uuid>', strict_slashes=False)
 api.add_resource(ActorListApi, '/actors', '/actors/<uuid>', strict_slashes=False)
 api.add_resource(AggregationApi, '/aggregations', strict_slashes=False)
